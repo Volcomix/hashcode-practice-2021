25e7db925c86cff51a9663653df92da8b389f3a3
Volcomix <Volcomix@users.noreply.github.com>
2021-02-24 14:43:24 +0100

    Print progress in browser console when profiling

--- profiling/solver.ts
+++ profiling/solver.ts
@@ -10,13 +10,21 @@ export async function solve(inputFile: File) {
   console.table(datasetInfo);
 
   const solverWorker = new SolverWorker(dataset, "greedy", "js");
+  const intervalId = setInterval(printProgress, 1000);
   await solverWorker.promise;
-
+  clearInterval(intervalId);
+  printProgress();
   if (solverWorker.submission) {
     console.table(
       [getSubmissionInfo(solverWorker.name, solverWorker.submission)],
     );
   } else {
     console.error("Failed to generate submission");
+  }
+
+  function printProgress() {
+    console.clear();
+    console.table(datasetInfo);
+    solverWorker.printProgress();
   }
 }
